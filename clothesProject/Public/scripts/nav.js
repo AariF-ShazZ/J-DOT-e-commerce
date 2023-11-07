beb3b6e95cae5986d4af4e864aaa870633c049bc
Aarif-shazZ <shazaarif0@gmail.com>
2023-11-07 12:14:41 +0530

    done

--- clothesProject/Public/scripts/nav.js
+++ clothesProject/Public/scripts/nav.js
@@ -15,8 +15,8 @@ const navFunction = () => {
     </div>
 </div>
 <ul class="links-container">
-    <li class="link-item"><a href="#" class="link">home</a></li>
-    <li class="link-item"><a href="#" class="link">women</a></li>
+    <li class="link-item"><a href="./index.html" class="link">home</a></li>
+    <li class="link-item"><a href="./product.html" class="link">women</a></li>
     <li class="link-item"><a href="#" class="link">men</a></li>
     <li class="link-item"><a href="#" class="link">kids</a></li>
     <li class="link-item"><a href="#" class="link">accessories</a></li>
